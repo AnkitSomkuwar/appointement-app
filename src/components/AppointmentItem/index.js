e4856d55b33fd5fa919fc6eb8a775cb98b0af6ab
AnkitSomkuwar <146408398+AnkitSomkuwar@users.noreply.github.com>
2024-03-10 12:54:47 +0530

    Update index.js

--- src/components/AppointmentItem/index.js
+++ src/components/AppointmentItem/index.js
@@ -1,1 +1,34 @@
 // Write your code here
+import {format} from 'date-fns'
+import './index.css'
+
+const AppointmentItem = props => {
+  const {appointmentDetails, toggleisStarred} = props
+  const {id, title, date, isStarred} = appointmentDetails
+  const starImgUrl = isStarred
+    ? 'https://assets.ccbp.in/frontend/react-js/appointments-app/filled-star-img.png'
+    : 'https://assets.ccbp.in/frontend/react-js/appointments-app/star-img.png'
+
+  const onClickStar = () => {
+    toggleisStarred(id)
+  }
+
+  return (
+    <li className="appointment-list">
+      <div className="title-star-container">
+        <p className="title">{title}</p>
+        <button
+          type="button"
+          className="star-btn"
+          data-testid="star"
+          onClick={onClickStar}
+        >
+          <img className="star-img" src={starImgUrl} alt="star" />
+        </button>
+      </div>
+      <p className="date">Date: {date}</p>
+    </li>
+  )
+}
+
+export default AppointmentItem
